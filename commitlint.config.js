c3cd2fc6e5aa2dba885ed4dd062fd869d6401f7f
Blueharmel <dldjdtjr@gmail.com>
2025-03-23 20:14:41 +0900

    youngseok, chore: Husky 관련 불필요한 요소 제거,정규식 & rules 개선 #16

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,18 +1,43 @@
 module.exports = {
     parserPreset: {
         parserOpts: {
-            headerPattern: /^(\w+), (\w+): (.+) #(\d+)$/,
-            headerCorrespondence: ['developer', 'branch', 'subject', 'issue']
+            headerPattern: /^(.+),\s+(.+):\s+(.+)\s+#(.+)$/,
+            headerCorrespondence: ['developer', 'type', 'subject', 'issue'],
+            issuePrefixes: ['#']
         }
     },
+    plugins: [
+        {
+            rules: {
+                'issue-format': ({ issue }) => {
+                    const issueNumber = issue.match(/(\d+)$/);
+                    return [
+                        issueNumber && parseInt(issueNumber[1]) > 0,
+                        `이슈 번호는 #(아라비아 숫자) 형식이어야 합니다 (예: #1)`
+                    ];
+                },
+                'developer-format': ({ developer }) => {
+                    const name = developer.match(/^(\w+)$/);
+                    return [
+                        name && name[1].length >= 3,
+                        `개발자 이름은 3자 이상 영문이어야 합니다 (예: byungchan)`
+                    ];
+                },
+                'type-enum': ({ type }) => {
+                    const allowedTypes = ['feat', 'hotfix', 'chore'];
+                    return [
+                        allowedTypes.includes(type),
+                        `타입은 ${allowedTypes.join(', ')} 중 하나여야 합니다.`
+                    ];
+                }
+            }
+        }
+    ],
     rules: {
-        'subject-empty': [2, 'never'], // 구현 내용이 비어 있으면 안 됨
-        'header-max-length': [2, 'always', 72], // 제목 길이는 최대 72자
-        'type-case': [2, 'always', 'lower-case'], // 브랜치명(타입)은 소문자여야 함
-        'type-enum': [
-            2,
-            'always',
-            ['feat', 'hotfix', 'chore'] // 허용되는 브랜치명 목록
-        ]
+        'header-max-length': [2, 'always', 72],
+        'developer-format': [2, 'always'],
+        'type-enum': [2, 'always'],
+        'subject-empty': [2, 'never'],
+        'issue-format': [2, 'always']
     }
 };
