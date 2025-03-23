612cb7158d55f98effa55cd7101e640e00210df7
Blueharmel <dldjdtjr@gmail.com>
2025-03-23 15:06:30 +0900

    youngseok, feat: 테이블 명을 복수로 변경 #12

--- src/main/java/com/househub/backend/domain/customer/entity/Customer.java
+++ src/main/java/com/househub/backend/domain/customer/entity/Customer.java
@@ -6,7 +6,7 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})})
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})}, name = "customers")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
