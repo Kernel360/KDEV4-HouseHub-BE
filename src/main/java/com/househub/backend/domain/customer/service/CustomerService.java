045d33f1c91fc729a0e6c795e4ae2e349934a29d
Blueharmel <dldjdtjr@gmail.com>
2025-03-27 18:35:11 +0900

    youngseok, feat: 불필요한 어노테이션 삭제 #12

--- src/main/java/com/househub/backend/domain/customer/service/CustomerService.java
+++ src/main/java/com/househub/backend/domain/customer/service/CustomerService.java
@@ -2,12 +2,10 @@ package com.househub.backend.domain.customer.service;
 
 import com.househub.backend.domain.customer.dto.CreateCustomerReqDto;
 import com.househub.backend.domain.customer.dto.CreateCustomerResDto;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-@Validated
 public interface CustomerService {
 
     CreateCustomerResDto createCustomer(CreateCustomerReqDto request);
