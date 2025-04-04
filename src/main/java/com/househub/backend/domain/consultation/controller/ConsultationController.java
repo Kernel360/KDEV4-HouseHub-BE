ab27dc8b18f7111f3845401f8f2708ebd0721a8a
cyunlee <siyunkr@gmail.com>
2025-04-04 14:58:29 +0900

    siyoon, chore: ConsultationController에 GlobalExcepttionHandler 적용 #25

--- src/main/java/com/househub/backend/domain/consultation/controller/ConsultationController.java
+++ src/main/java/com/househub/backend/domain/consultation/controller/ConsultationController.java
@@ -1,7 +1,5 @@
 package com.househub.backend.domain.consultation.controller;
 
-import com.househub.backend.common.exception.ValidationFailedException;
-import com.househub.backend.common.response.ErrorResponse;
 import com.househub.backend.common.response.SuccessResponse;
 import com.househub.backend.domain.consultation.dto.ConsultationReqDto;
 import com.househub.backend.domain.consultation.dto.ConsultationResDto;
@@ -9,11 +7,9 @@ import com.househub.backend.domain.consultation.service.ConsultationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,12 +20,8 @@ public class ConsultationController {
 
     @PostMapping("")
     public ResponseEntity<SuccessResponse<ConsultationResDto>> createConsultation(
-        @Valid
-        @RequestBody ConsultationReqDto consultationReqDto,
-        BindingResult bindingResult
+            @Valid @RequestBody ConsultationReqDto consultationReqDto
     ) {
-        validateRequest(bindingResult);
-
         ConsultationResDto response = consultationService.create(consultationReqDto);
         return ResponseEntity.ok(SuccessResponse.success("상담 등록이 완료되었습니다.", "CONSULTATION_REGISTER_SUCCESS", response));
     }
@@ -41,46 +33,23 @@ public class ConsultationController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<SuccessResponse<ConsultationResDto>> findOneConsultation(
-            @PathVariable Long id
-    ) {
+    public ResponseEntity<SuccessResponse<ConsultationResDto>> findOneConsultation(@PathVariable Long id) {
         ConsultationResDto response = consultationService.findOne(id);
         return ResponseEntity.ok(SuccessResponse.success("상담 상세 조회에 성공했습니다.", "FIND_CONSULTATION_SUCCESS", response));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<SuccessResponse<ConsultationResDto>> updateConsultation(
-        @Valid
-        @RequestBody ConsultationReqDto consultationReqDto,
-        BindingResult bindingResult,
-        @PathVariable Long id
+            @Valid @RequestBody ConsultationReqDto consultationReqDto,
+            @PathVariable Long id
     ) {
-        validateRequest(bindingResult);
-
         ConsultationResDto response = consultationService.update(id, consultationReqDto);
         return ResponseEntity.ok(SuccessResponse.success("상담 정보 수정에 성공했습니다.", "UPDATE_CONSULTATION_SUCCESS", response));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<SuccessResponse<ConsultationResDto>> deleteConsultation(
-        @PathVariable Long id
-    ) {
+    public ResponseEntity<SuccessResponse<ConsultationResDto>> deleteConsultation(@PathVariable Long id) {
         ConsultationResDto response = consultationService.delete(id);
-
         return ResponseEntity.ok(SuccessResponse.success("상담 정보 삭제에 성공했습니다.", "DELETE_CONSULTATION_SUCCESS", response));
-    }
-
-    private void validateRequest(BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            List<ErrorResponse.FieldError> errors = bindingResult.getFieldErrors()
-                    .stream()
-                    .map(error -> ErrorResponse.FieldError.builder()
-                            .field(error.getField())
-                            .message(error.getDefaultMessage())
-                            .build())
-                    .collect(Collectors.toList());
-
-            throw new ValidationFailedException("유효성 검사 실패", errors, "VALIDATION_FAILED");
-        }
     }
 }
