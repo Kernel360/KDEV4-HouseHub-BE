7b8cd86b67b13baecc89020b73686a5c8ff2566d
heoseongeun <rhdwnsla1223@naver.com>
2025-04-18 12:17:29 +0900

    seongeun, fix: 매물 서비스 테스트 코드 작성 #13

--- src/test/java/com/househub/backend/domain/property/service/PropertyServiceTest.java
+++ src/test/java/com/househub/backend/domain/property/service/PropertyServiceTest.java
@@ -1,41 +1,24 @@
 package com.househub.backend.domain.property.service;
 
-import static org.assertj.core.api.AssertionsForClassTypes.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
-import com.househub.backend.common.exception.AlreadyExistsException;
-import com.househub.backend.common.exception.ResourceNotFoundException;
 import com.househub.backend.domain.agent.entity.Agent;
 import com.househub.backend.domain.agent.entity.AgentStatus;
 import com.househub.backend.domain.agent.repository.AgentRepository;
-import com.househub.backend.domain.contract.entity.Contract;
 import com.househub.backend.domain.customer.entity.Customer;
 import com.househub.backend.domain.customer.repository.CustomerRepository;
-import com.househub.backend.domain.property.dto.CreatePropertyResDto;
-import com.househub.backend.domain.property.dto.FindPropertyDetailResDto;
-import com.househub.backend.domain.property.dto.PropertyListResDto;
 import com.househub.backend.domain.property.dto.PropertyReqDto;
-import com.househub.backend.domain.property.dto.PropertySearchDto;
+import com.househub.backend.domain.property.dto.PropertyUpdateReqDto;
 import com.househub.backend.domain.property.entity.Property;
-import com.househub.backend.domain.property.enums.PropertyType;
-import com.househub.backend.domain.property.repository.PropertyRepository;
 import com.househub.backend.domain.property.service.impl.PropertyServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,7 +27,10 @@ public class PropertyServiceTest {
 	private PropertyServiceImpl propertyService;
 
 	@Mock
-	private PropertyRepository propertyRepository;
+	private PropertyReader propertyReader;
+
+	@Mock
+	private PropertyStore propertyStore;
 
 	@Mock
 	private CustomerRepository customerRepository;
@@ -52,176 +38,82 @@ public class PropertyServiceTest {
 	@Mock
 	private AgentRepository agentRepository;
 
-	private Agent agent;
-	private Customer customer;
-	private Property property;
+	@Mock
+	private PropertyConditionService propertyConditionService;
+
+	private Agent mockAgent;
+	private Customer mockCustomer;
+	private Property mockProperty;
 
 	@BeforeEach
 	void setUp() {
-		agent = Agent.builder()
+		mockAgent = Agent.builder().id(1L).status(AgentStatus.ACTIVE).build();
+		mockCustomer = Customer.builder().id(1L).build();
+		mockProperty = Property.builder()
 			.id(1L)
-			.status(AgentStatus.ACTIVE)
-			.build();
-
-		customer = Customer.builder()
-			.id(1L)
-			.build();
-
-		property = Property.builder()
-			.id(1L)
-			.agent(agent)
-			.customer(customer)
-			.contracts(new ArrayList<>())
+			.customer(mockCustomer)
+			.agent(mockAgent)
+			.roadAddress("서울시 도로명")
+			.detailAddress("상세 주소")
 			.build();
 	}
 
 	@Test
-	@DisplayName("매물 등록 성공")
-	void createProperty_Success() {
-		PropertyReqDto dto = PropertyReqDto.builder()
-			.customerId(1L)
-			.roadAddress("서울특별시 강남구 테헤란로 123")
-			.jibunAddress("서울특별시 강남구 삼성동 123-45")
-			.detailAddress("101동 302호")
-			.build();
-		when(agentRepository.findByIdAndStatus(1L, AgentStatus.ACTIVE)).thenReturn(Optional.of(agent));
-		when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
-		when(propertyRepository.existsByRoadAddressAndDetailAddress(dto.getRoadAddress(),
-			dto.getDetailAddress())).thenReturn(false);
-		when(propertyRepository.save(any(Property.class))).thenReturn(property);
-
-		// 매물 등록 메서드 호출
-		CreatePropertyResDto response = propertyService.createProperty(dto, 1L);
-
-		assertNotNull(response);
-	}
-
-	@Test
-	@DisplayName("동일한 주소를 가진 매물을 등록할 경우 - 예외 처리")
-	void createProperty_ThrowsAlreadyExistsException() {
-		PropertyReqDto dto = PropertyReqDto.builder()
-			.customerId(1L)
-			.propertyType(PropertyType.APARTMENT)
-			.roadAddress("서울특별시 강남구 테헤란로 123")
-			.jibunAddress("서울특별시 강남구 삼성동 123-45")
-			.detailAddress("101동 302호")
-			.build();
-		when(agentRepository.findByIdAndStatus(1L, AgentStatus.ACTIVE)).thenReturn(Optional.of(agent));
-		when(propertyRepository.existsByRoadAddressAndDetailAddress(dto.getRoadAddress(),
-			dto.getDetailAddress())).thenReturn(true);
-
-		assertThrows(AlreadyExistsException.class, () -> propertyService.createProperty(dto, 1L));
-	}
-
-	@Test
-	@DisplayName("매물 상세 조회 성공")
-	void findProperty_Success() {
-		when(propertyRepository.findById(1L)).thenReturn(Optional.of(property));
-
-		FindPropertyDetailResDto response = propertyService.findProperty(1L);
-
-		assertNotNull(response);
-		assertEquals(1L, response.getId());
-	}
-
-	@Test
-	@DisplayName("존재하지 않는 매물인 경우 - 예외 처리")
-	void findProperty_ThrowsResourceNotFoundException() {
-		when(propertyRepository.findById(1L)).thenReturn(Optional.empty());
-
-		assertThrows(ResourceNotFoundException.class, () -> propertyService.findProperty(1L));
-	}
-
-	@Test
-	@DisplayName("전체 매물 조회 성공")
-	void findProperties_Success() {
+	void createPropertyWithoutConditions() {
+		// given
 		Long agentId = 1L;
+		Long customerId = 2L;
 
-		PropertySearchDto searchDto = PropertySearchDto.builder()
-			.province("서울")
-			.city("강남구")
-			.dong("역삼동")
-			.propertyType(PropertyType.APARTMENT)
-			.agentName("홍길동")
-			.customerName("김철수")
-			.active(true)
-			.build();
+		PropertyReqDto reqDto = PropertyReqDto.builder()
+			.customerId(customerId)
+			.roadAddress("서울시 강남구 테헤란로 123")
+			.jibunAddress("서울시 강남구 서초동 123-45")
+			.detailAddress("101동 101호")
+			// .conditions(List.of(new PropertyConditionReqDto(...))) // 적절한 값 넣기
+            .build();
 
-		Pageable pageable = PageRequest.of(0, 10);
+		Agent agent = Agent.builder().id(agentId).build();
+		Customer customer = Customer.builder().id(customerId).build();
+		Property property = mock(Property.class); // 또는 reqDto.toEntity(customer, agent)
 
-		Property mockProperty = Property.builder()
-			.id(1L)
-			.propertyType(PropertyType.APARTMENT)
-			.build();
-
-		Page<Property> propertyPage = new PageImpl<>(List.of(mockProperty), pageable, 1);
-
-		when(propertyRepository.searchProperties(
-			eq(agentId),
-			eq("서울"),
-			eq("강남구"),
-			eq("역삼동"),
-			eq(PropertyType.APARTMENT),
-			eq("홍길동"),
-			eq("김철수"),
-			eq(true),
-			eq(pageable)
-		)).thenReturn(propertyPage);
+		given(agentRepository.findByIdAndStatus(agentId, AgentStatus.ACTIVE))
+			.willReturn(Optional.of(agent));
+		given(customerRepository.findById(customerId))
+			.willReturn(Optional.of(customer));
+		willDoNothing().given(propertyReader)
+			.validateRegisterProperty(anyString(), anyString(), eq(customerId));
+		given(propertyStore.store(any(Property.class))).willReturn(property);
 
 		// when
-		PropertyListResDto result = propertyService.findProperties(searchDto, pageable, agentId);
+		propertyService.createProperty(reqDto, agentId);
 
 		// then
-		assertThat(result).isNotNull();
-
-		verify(propertyRepository, times(1)).searchProperties(
-			eq(agentId),
-			eq("서울"),
-			eq("강남구"),
-			eq("역삼동"),
-			eq(PropertyType.APARTMENT),
-			eq("홍길동"),
-			eq("김철수"),
-			eq(true),
-			eq(pageable)
-		);
+		verify(agentRepository).findByIdAndStatus(agentId, AgentStatus.ACTIVE);
+		verify(customerRepository).findById(customerId);
+		verify(propertyReader).validateRegisterProperty(any(), any(), any());
+		verify(propertyStore).store(any(Property.class));
+		// verify(propertyConditionService).createPropertyCondition(anyLong(), any(PropertyConditionReqDto.class));
 	}
 
 	@Test
-	@DisplayName("매물 정보 수정 성공")
-	void updateProperty_Success() {
-		PropertyReqDto updateDto = PropertyReqDto.builder()
+	void updatePropertySuccess() {
+		// given
+		Long propertyId = 1L;
+		PropertyUpdateReqDto updateDto = PropertyUpdateReqDto.builder()
 			.customerId(1L)
-			.roadAddress("서울특별시 강남구 테헤란로 123")
-			.jibunAddress("서울특별시 강남구 삼성동 123-45")
-			.detailAddress("101동 302호")
+			.roadAddress("대전 동구 옥천로 123")
+			.jibunAddress("대전 동구 옥천동 123-45")
+			.detailAddress("변경된 상세주소")
 			.build();
-		// when(propertyRepository.existsByRoadAddressAndDetailAddress(updateDto.getRoadAddress(),
-		// 	updateDto.getDetailAddress())).thenReturn(false);
-		when(customerRepository.findById(1L)).thenReturn(Optional.of(customer));
-		when(propertyRepository.findById(1L)).thenReturn(Optional.of(property));
 
-		propertyService.updateProperty(1L, updateDto);
+		when(propertyReader.findPropertyBy(propertyId)).thenReturn(mockProperty);
+		when(customerRepository.findById(1L)).thenReturn(Optional.of(mockCustomer));
 
-		verify(propertyRepository, times(1)).findById(1L);
-	}
+		// when
+		propertyService.updateProperty(propertyId, updateDto);
 
-	@Test
-	@DisplayName("매물 삭제 성공")
-	void deleteProperty_Success() {
-		Contract contract1 = Contract.builder().id(1L).property(property).build();
-		Contract contract2 = Contract.builder().id(2L).property(property).build();
-		property = Property.builder()
-			.id(1L)
-			.contracts(List.of(contract1, contract2)) // 매물에 계약 2개 추가
-			.build();
-		when(propertyRepository.findById(1L)).thenReturn(Optional.of(property));
-
-		// when: 매물 삭제 메서드 실행
-		propertyService.deleteProperty(1L);
-
-		// then: 매물과 모든 계약이 소프트 삭제되었는지 검증
-		assertNotNull(property.getDeletedAt());
-		property.getContracts().forEach(contract -> assertNotNull(contract.getDeletedAt()));
+		// then
+		verify(propertyReader).validateRegisterProperty(anyString(), anyString(), anyLong());
+		verify(customerRepository).findById(1L);
 	}
 }
