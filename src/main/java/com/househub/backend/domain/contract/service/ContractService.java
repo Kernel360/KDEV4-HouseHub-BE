df1d2841555aafc9c0f0c951f5aec87e8cfbbb97
Blueharmel <dldjdtjr@gmail.com>
2025-04-25 11:43:59 +0900

    youngseok, fix: 불필요한 import 제거 #145

--- src/main/java/com/househub/backend/domain/contract/service/ContractService.java
+++ src/main/java/com/househub/backend/domain/contract/service/ContractService.java
@@ -1,32 +1,35 @@
 package com.househub.backend.domain.contract.service;
 
-import com.househub.backend.domain.contract.dto.*;
-import com.househub.backend.domain.property.entity.Property;
-
 import java.util.List;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.househub.backend.domain.contract.dto.ContractListResDto;
+import com.househub.backend.domain.contract.dto.ContractReqDto;
+import com.househub.backend.domain.contract.dto.ContractSearchDto;
+import com.househub.backend.domain.contract.dto.CreateContractResDto;
+import com.househub.backend.domain.contract.dto.FindContractResDto;
+import com.househub.backend.domain.property.entity.Property;
+
 public interface ContractService {
-    // 계약 등록
-    public CreateContractResDto createContract(ContractReqDto contractReqDto, Long agentId);
+	// 계약 등록
+	public CreateContractResDto createContract(ContractReqDto contractReqDto, Long agentId);
 
-    // 계약 수정
-    public void updateContract(Long id, ContractReqDto dto);
+	// 계약 수정
+	public void updateContract(Long id, ContractReqDto dto);
 
-    // 계약 목록 조회
-    public ContractListResDto findContracts(ContractSearchDto searchDto, Pageable pageable, Long agentId);
+	// 계약 목록 조회
+	public ContractListResDto findContracts(ContractSearchDto searchDto, Pageable pageable, Long agentId);
 
-    // 고객 계약 내역 조회
-    public ContractListResDto findAllByCustomer(Long id, Pageable pageable, Long agentId);
+	// 고객 계약 내역 조회
+	public ContractListResDto findAllByCustomer(Long id, Pageable pageable, Long agentId);
 
-    // 매물 계약 내역 조회
-    public ContractListResDto findAllByProperties(List<Property> propertyIds, Pageable pageable, Long agentId);
+	// 매물 계약 내역 조회
+	public ContractListResDto findAllByProperties(List<Property> propertyIds, Pageable pageable, Long agentId);
 
-    // 계약 상세 조회
-    public FindContractResDto findContract(Long id);
+	// 계약 상세 조회
+	public FindContractResDto findContract(Long id);
 
-    // 계약 삭제
-    public void deleteContract(Long id);
+	// 계약 삭제
+	public void deleteContract(Long id);
 }
