4ea3fede12eda25e528b54dddd326394a50d5ee9
heoseongeun <rhdwnsla1223@naver.com>
2025-04-29 12:10:32 +0900

    seongeun, feat: 계약 가격을 억/만 단위로 변환해주는 formatPrice 메서드 구현 #133

--- src/main/java/com/househub/backend/domain/contract/dto/FindContractResDto.java
+++ src/main/java/com/househub/backend/domain/contract/dto/FindContractResDto.java
@@ -26,10 +26,10 @@ public class FindContractResDto {
     // private Integer monthlyRentFee; // 월세 금액
     // private Integer monthlyRentDeposit; // 월세 보증금
 
-    private Long salePrice; // 매매가
-    private Long jeonsePrice; // 전세가
-    private Integer monthlyRentFee; // 월세 금액
-    private Integer monthlyRentDeposit; // 월세 보증금
+    private String salePrice; // 매매가
+    private String jeonsePrice; // 전세가
+    private String monthlyRentFee; // 월세 금액
+    private String monthlyRentDeposit; // 월세 보증금
 
     private ContractStatus status; // 계약 상태 (ON_SALE, SOLD_OUT)
     private String memo; // 계약 관련 메모
@@ -47,10 +47,10 @@ public class FindContractResDto {
                 .customer(contract.getCustomer() != null ?
                         CreateCustomerResDto.fromEntity(contract.getCustomer()) : null)
                 .contractType(contract.getContractType())
-                .salePrice(contract.getSalePrice())
-                .jeonsePrice(contract.getJeonsePrice())
-                .monthlyRentFee(contract.getMonthlyRentFee())
-                .monthlyRentDeposit(contract.getMonthlyRentDeposit())
+                .salePrice(convertPriceFormat(contract.getSalePrice()))
+                .jeonsePrice(convertPriceFormat(contract.getJeonsePrice()))
+                .monthlyRentFee(convertPriceFormat(contract.getMonthlyRentFee()))
+                .monthlyRentDeposit(convertPriceFormat(contract.getMonthlyRentDeposit()))
                 .status(contract.getStatus())
                 .memo(contract.getMemo())
                 .startedAt(contract.getStartedAt())
@@ -60,6 +60,41 @@ public class FindContractResDto {
                 .build();
     }
 
+    private static String convertPriceFormat(Long price) {
+        if (price == null) {
+            return "-";
+        }
 
+        long eok = price / 100_000_000; // 억 단위
+        long man = (price % 100_000_000) / 10_000; // 억을 제외한 나머지 만 단위
+
+        StringBuilder sb = new StringBuilder();
+
+        if (eok > 0) {
+            sb.append(eok).append("억 ");
+        }
+
+        if (man > 0) {
+            sb.append(String.format("%,d", man)).append("만");
+        }
+
+        if (sb.length() == 0) {
+            // 억, 만 둘 다 0인 경우 (이론상 거의 없지만)
+            return "0";
+        }
+
+        return sb.toString().trim();
+    }
+
+    private static String addComma(String price) {
+        String result;
+
+        if (price.length() == 4) {
+            result = price.substring(0, 1) + "," + price.substring(1);
+        } else {
+            result = price;
+        }
+        return result;
+    }
 
 }
